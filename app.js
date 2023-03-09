188ebdc8bfb00146b68b1a8e7101415a59642808
Hank <billingran@gmail.com>
2023-03-10 00:31:42 +0100

    2023/03/09

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
+const expressLayouts = require("express-ejs-layouts");
 const Category = require("./src/models/category_model");
 const User = require("./src/models/user_model");
 const Course = require("./src/models/course_model");
@@ -9,15 +10,23 @@ const Course = require("./src/models/course_model");
 require("./src/models/database");
 
 //Middleware
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Middleware of ejs layouts and ejs
+app.use(expressLayouts);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 // Middleware of routes
 
 app.get("/", (req, res) => {
-  res.send("hi");
+  res.render("index", { title: "Me" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(8080, () => {
