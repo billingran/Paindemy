80c25a9fc7445d735576c2060ab1bea760f76f81
Hank <billingran@gmail.com>
2023-03-17 00:55:31 +0100

    2023/03/16

--- src/controllers/auth_controller.js
+++ src/controllers/auth_controller.js
@@ -7,3 +7,8 @@ module.exports.signUp = (req, res) => {
 module.exports.login = (req, res) => {
   res.render("login", { title: "Login" });
 };
+
+// join us
+module.exports.joinUs = (req, res) => {
+  res.render("join_us", { title: "Join Us" });
+};
--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -4,6 +4,11 @@ const authController = require("../controllers/auth_controller");
 
 // sign up
 router.get("/signup", authController.signUp);
+
+// login
 router.get("/login", authController.login);
+
+// join us
+router.get("/joinus", authController.joinUs);
 
 module.exports = router;
