51c1c7de53341f114d4c90750d700067714581aa
Hank <billingran@gmail.com>
2023-04-01 09:38:59 +0200

    2023/04/01

--- public/js/header.js
+++ public/js/header.js
@@ -3,22 +3,19 @@ let headerBackground = document.querySelector(".header");
 let ctnHeader = document.querySelector(".ctn_header");
 let formHomeOne = document.querySelector(".form_homeOne");
 let iconSearchHomeOne = document.querySelector(".icon_search_homeOne");
-let inputHomeOne = document.querySelector(".input_homeOne");
 
 function turnNavColor() {
   if (window.pageYOffset) {
     headerBackground.style =
       "background-color:rgba(220,164,125,0.7); box-shadow: 0 8px 6px -6px rgba(0,0,0,0.3);";
     ctnHeader.style = "border: none";
-    formHomeOne.style = "border: 1px solid var(--C-6E1D0E)";
+    formHomeOne.style = "background: rgba(243,243,243,0.7) ";
     iconSearchHomeOne.style = "color:var(--C-6E1D0E)";
-    inputHomeOne.style = "color:var(--C-6E1D0E)";
   } else {
     headerBackground.style = "";
     ctnHeader.style = "";
     formHomeOne.style = "";
     iconSearchHomeOne.style = "";
-    inputHomeOne.style = "";
   }
 }
 
