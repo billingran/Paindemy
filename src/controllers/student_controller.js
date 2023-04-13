000c6320cbec718b7173c6f1a96c929177081e83
Hank <billingran@gmail.com>
2023-04-13 17:30:37 +0200

    2023/04/13

--- src/controllers/student_controller.js
+++ src/controllers/student_controller.js
@@ -48,9 +48,41 @@ module.exports.postStudentDelete = async (req, res) => {
 
 //student my courses
 module.exports.studentMycourses = async (req, res) => {
-  res.render("my_courses", {
-    title: "Student my courses",
-    showHeader: true,
-    authUser: req.user,
-  });
+  try {
+    let { _id } = req.params;
+
+    ////////////////////////////////////////////////////
+    // add student into the course
+
+    const course = await Course.findOne({ _id }).exec();
+    console.log(course.studentsCourse.includes(req.user._id));
+
+    if (course.studentsCourse.includes(req.user._id)) {
+      req.flash("error_msg", "You can't join same course two times");
+      return res.redirect(`/student/mycourses/${req.user._id}`);
+    }
+
+    await Course.updateOne(
+      { _id },
+      { $push: { studentsCourse: req.user._id } },
+      { runValidators: true }
+    ).exec();
+
+    // let test = await Course.updateOne(
+    //   { _id },
+    //   { $addToSet: { studentsCourse: req.user._id } },
+    //   { runValidators: true }
+    // ).exec();
+
+    // console.log(test);
+
+    res.render("my_courses", {
+      title: "Student my courses",
+      showHeader: true,
+      authUser: req.user,
+    });
+  } catch (error) {
+    return res.status(500).send(error);
+    console.log(error);
+  }
 };
