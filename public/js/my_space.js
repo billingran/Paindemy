9913427fb7081bec02882852869b5a10da3adecf
Hank <billingran@gmail.com>
2023-04-25 23:35:55 +0200

    2023/04/25

--- public/js/my_space.js
+++ public/js/my_space.js
@@ -8,15 +8,17 @@ function addAndTransformPercentage(event) {
   const percentageLabel =
     event.target.parentElement.nextElementSibling.querySelector("label");
 
-  let valuePercentageIngredient = event.target.value;
+  let value = event.target.value;
+  let valuePercentageIngredient = Number(value);
 
   if (!isNaN(valuePercentageIngredient)) {
     percentageLabel.textContent = (
-      (Number(valuePercentageIngredient) / 1) *
+      (valuePercentageIngredient / 1) *
       100
     ).toFixed(2);
   } else {
     percentageLabel.textContent = (0).toFixed(2);
+    event.target.value = (0).toFixed(2);
   }
 }
 
@@ -24,3 +26,29 @@ percentageIngredients.forEach((percentageIngredient) => {
   // add and transform for percentage label
   percentageIngredient.addEventListener("input", addAndTransformPercentage);
 });
+
+// caculate result percentage for each ingredient
+function calculatePercentage(event) {
+  // get quantity dough
+  let quantityDough = event.target.value;
+  // get total percentage
+  let totalPercentage = 0;
+
+  percentageIngredients.forEach((percentageIngredient) => {
+    let value = percentageIngredient.value;
+    let valuePercentage = Number(value);
+
+    // get total percentage
+    if (!isNaN(valuePercentage) && !isNaN(quantityDough)) {
+      totalPercentage += valuePercentage;
+
+      let resultPercentage =
+        (valuePercentage / totalPercentage) * quantityDough;
+
+      console.log(resultPercentage.toFixed(3));
+    }
+  });
+}
+
+// caculate result percentage for each ingredient
+quantityDough.addEventListener("input", calculatePercentage);
